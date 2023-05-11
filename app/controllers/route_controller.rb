8057332488d4cfcbd635b87b96594d9b7fcf0e7c
Dean Wan <deanwan14@gmail.com>
2023-05-11 22:38:15 +1000

    feat: add error handling for non-recognised routes

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -3,6 +3,11 @@ class RouteController < ApplicationController
     render json: { path: parsed_path }
   end
 
+  def unrecognised_endpoint
+    payload = {error: "Endpoint not recognised"}
+    render :json => payload, :status => :not_found
+  end
+
   private
 
   def parsed_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'users/' => 'route#index'
   get 'users/*path' => 'route#index'
+
+  match '*unmatched', to: 'route#unrecognised_endpoint', via: :get
 end
