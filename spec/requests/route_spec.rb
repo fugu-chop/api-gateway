b3b1cb3a797fb345b60254b2b9e3b2c9fdaf7eb1
Dean Wan <deanwan14@gmail.com>
2023-05-11 22:20:45 +1000

    test: add specs for get requests

--- spec/requests/route_spec.rb
+++ spec/requests/route_spec.rb
@@ -1,7 +1,39 @@
-require 'rails_helper'
+require 'rails_helper'		
 
 RSpec.describe "Routes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    get "/users/#{path}"
+  end
+
+  describe "GET /users" do
+    context "when the request is valid" do
+      let(:path) { "abcdefg" }
+
+      it "returns a JSON" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+    end
+
+    context "when there is nothing else in the path" do 
+      let(:path) { "" }
+
+      it "has no additional information in the request body" do
+        json_body = JSON.parse(response.body)
+        
+        expect(json_body).to have_key("path")
+        expect(json_body["path"]).to eq ""
+      end
+    end
+
+    context "when there is additional info in the path" do
+      let(:path) { "123/456" }
+
+      it "captures all subsequent path information in the request body" do        
+        json_body = JSON.parse(response.body)
+        
+        expect(json_body).to have_key("path")
+        expect(json_body["path"]).to include path
+      end
+    end
   end
 end
