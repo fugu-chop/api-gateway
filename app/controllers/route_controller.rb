33d5948b04a86b0075947ba4b5593c20fe2ae564
Dean Wan <deanwan14@gmail.com>
2023-05-12 23:31:06 +1000

    feat: handle root route

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -2,11 +2,17 @@
 
 class RouteController < ApplicationController
   def index
-    render json: { path: params[:path] }
+    render json: { path: parsed_path }
   end
 
   def unrecognised_endpoint
     payload = { error: 'Endpoint not recognised' }
     render json: payload, status: :not_found
+  end
+
+  private
+
+  def parsed_path
+    params[:path] || ''
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'route#index'
   match '*path', to: 'route#index', via: :get
 end
