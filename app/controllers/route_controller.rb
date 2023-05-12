e28982f082ad77d9a48047f2cd69dcdaec68041f
Dean Wan <deanwan14@gmail.com>
2023-05-12 17:53:29 +1000

    refactor: simplify path logic

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -2,17 +2,11 @@
 
 class RouteController < ApplicationController
   def index
-    render json: { path: parsed_path }
+    render json: { path: params[:path] }
   end
 
   def unrecognised_endpoint
     payload = { error: 'Endpoint not recognised' }
     render json: payload, status: :not_found
-  end
-
-  private
-
-  def parsed_path
-    params[:path] || ''
   end
 end
