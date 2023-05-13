08426b546b4b17c476fff3c1d53f223dcb65eb3a
Dean Wan <deanwan14@gmail.com>
2023-05-13 13:40:35 +1000

    style: lint

--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class RouteController < ApplicationController
-  ROUTE_TABLE_LOCATION = "#{Rails.root.to_s}/config/gateway_routes.yml"
-  UNRECOGNISED_ENDPOINT_MSG = "Endpoint not recognised"
+  ROUTE_TABLE_LOCATION = "#{Rails.root}/config/gateway_routes.yml".freeze
+  UNRECOGNISED_ENDPOINT_MSG = 'Endpoint not recognised'
 
   def index
     return unrecognised_endpoint if parsed_path == UNRECOGNISED_ENDPOINT_MSG
 
     uri = URI(parsed_path)
-    # This does not handle /users/ as Net::HTTP does not handle 3xx 
+    # This does not handle /users/ as Net::HTTP does not handle 3xx
     # redirects, which dummyjson responds with for the /users endpoint
     response = Net::HTTP.get(uri)
     render json: response
@@ -21,13 +21,13 @@ class RouteController < ApplicationController
   end
 
   def parsed_path
-    routes = YAML::load(File.open(ROUTE_TABLE_LOCATION))
-    service, *resource = initial_path.split("/") 
+    routes = YAML.safe_load(File.open(ROUTE_TABLE_LOCATION))
+    service, *resource = initial_path.split('/')
     redirected_route = routes[service]
 
     if redirected_route
-      redirected_route + "/" + resource.join('/')
-    else    
+      "#{redirected_route}/#{resource.join('/')}"
+    else
       UNRECOGNISED_ENDPOINT_MSG
     end
   end
