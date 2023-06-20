d1d3a13b029aa9810299500ceb87ac4c783fdc98
Dean Wan <deanwan14@gmail.com>
2023-06-20 23:04:44 +1000

    chore: set up seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+routes_location = "#{Rails.root}/config/gateway_routes.yml".freeze
+routes_hash = YAML.safe_load(File.open(routes_location))
+routes_hash.each do |path, route|
+  Route.create(path: path, route: route)
+end
